b9f0fd8438295cceb57bea11eb4fc8dc07817265
Ken Nakanishi <ikyhn1.ken.n@gmail.com>
2018-04-06 23:38:18 +0900

    bug fix

--- main.py
+++ main.py
@@ -43,45 +43,34 @@ class Qubits:
         if isinstance(control, int):
             control = (control,)
 
-        self.data = xp.array(self.data).astype(self.dtype)
-        operator = xp.array(operator).astype(self.dtype)
-
-        # for i in range(int(math.log2(operator.shape[0])) - 1, 0, -1):
-        #     operator = np.asarray([np.split(_o, operator.shape[0] // 2, axis=1) for _o in
-        #                            np.split(operator, operator.shape[0] // 2, axis=0)])
-
-        self.data = self.data.reshape([2] * int(math.log2(self.data.size)))
-        operator = operator.reshape([2] * int(math.log2(operator.size)))
-
-        # print(operator.real.astype(int))
-
-        # if (2 ** int(math.log2(operator.ndim)) != operator.ndim) or operator.ndim < 2:
-        #     print('Operator dimention must be 2^n (n=1,2,3,...)')
-        #     print('{} is not 2^n (n=1,2,3,...)'.format(operator.ndim))
-        #     sys.exit(-1)
+        self.data = xp.array(self.data).astype(self.dtype).reshape([2] * int(math.log2(self.data.size)))
+        operator = xp.array(operator).astype(self.dtype).reshape([2] * int(math.log2(operator.size)))
 
         if operator.ndim != len(target) * 2:
             print('You must set operator.size==exp(len(target)*2)')
             sys.exit(-1)
 
-        c_index = [slice(None)] * self.size
+        c_slice = [slice(None)] * self.size
         if control is not None:
             for _c in control:
-                c_index[_c] = slice(1, 2)
+                c_slice[_c] = slice(1, 2)
 
+        c_index = list(range(self.size))
         t_index = list(range(self.size))
         for i, _t in enumerate(target):
             t_index[_t] = self.size + i
         o_index = [*target, *list(range(self.size, self.size + len(target)))]
-        # o_index = o_index[0::3] + o_index[1::3] + o_index[2::3]
 
-        # print(o_index)
-        # print(c_index)
-        # print(t_index)
+        # use following code when numpy bug is removed
+        # self.data[c_slice] = xp.einsum(operator, o_index, self.data[c_slice], c_index, t_index)
 
-        self.data[c_index] = xp.einsum(operator, o_index,
-                                       self.data[c_index], list(range(self.size)),
-                                       t_index)
+        # alternative code
+        character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        o_index = ''.join([character[i] for i in o_index])
+        c_index = ''.join([character[i] for i in c_index])
+        t_index = ''.join([character[i] for i in t_index])
+        subscripts = '{},{}->{}'.format(o_index, c_index, t_index)
+        self.data[c_slice] = xp.einsum(subscripts, operator, self.data[c_slice])
 
     def projection(self, target):
         xp = self.xp
@@ -101,33 +90,32 @@ class Qubits:
             print('shape: {}'.format(self.data.shape))
 
 
-I = np.array([[1, 0], [0, 1]])
-X = np.array([[0, 1], [1, 0]])
-Y = np.array([[0, -1j], [1j, 0]])
-Z = np.array([[1, 0], [0, -1]])
-H = np.array([[1, 1], [1, -1]]) / math.sqrt(2)
-S = np.array([[1, 0], [0, 1j]])
-
-rx = lambda phi: math.cos(phi/2) * I - 1j * math.sin(phi/2) * X
-ry = lambda phi: math.cos(phi/2) * I - 1j * math.sin(phi/2) * Y
-rz = lambda phi: math.cos(phi/2) * I - 1j * math.sin(phi/2) * Z
-
-sqrt_not = np.array([[1 + 1j, 1 - 1j], [1 - 1j, 1 + 1j]]) / 2
-phase = lambda phi: np.array([[1, 0], [0, cmath.exp(1j * phi)]])
-g_phase = lambda phi: cmath.exp(1j * phi)
-
-swap = np.array([[1, 0, 0, 0],
-                 [0, 0, 1, 0],
-                 [0, 1, 0, 0],
-                 [0, 0, 0, 1]])
-
-iswap = np.array([[1,  0,  0, 0],
-                  [0,  0, 1j, 0],
-                  [0, 1j,  0, 0],
-                  [0,  0,  0, 1]])
-
-
 if __name__ == '__main__':
+    I = np.array([[1, 0], [0, 1]])
+    X = np.array([[0, 1], [1, 0]])
+    Y = np.array([[0, -1j], [1j, 0]])
+    Z = np.array([[1, 0], [0, -1]])
+    H = np.array([[1, 1], [1, -1]]) / math.sqrt(2)
+    S = np.array([[1, 0], [0, 1j]])
+
+    rx = lambda phi: math.cos(phi / 2) * I - 1j * math.sin(phi / 2) * X
+    ry = lambda phi: math.cos(phi / 2) * I - 1j * math.sin(phi / 2) * Y
+    rz = lambda phi: math.cos(phi / 2) * I - 1j * math.sin(phi / 2) * Z
+
+    sqrt_not = np.array([[1 + 1j, 1 - 1j], [1 - 1j, 1 + 1j]]) / 2
+    phase = lambda phi: np.array([[1, 0], [0, cmath.exp(1j * phi)]])
+    g_phase = lambda phi: cmath.exp(1j * phi)
+
+    swap = np.array([[1, 0, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 0, 1]])
+
+    iswap = np.array([[1, 0, 0, 0],
+                      [0, 0, 1j, 0],
+                      [0, 1j, 0, 0],
+                      [0, 0, 0, 1]])
+
     q = Qubits(3)
     q.gate(H, target=0)
     q.gate(H, target=1)
