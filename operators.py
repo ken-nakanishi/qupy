d0666068ee709ccba7fcc55dcf08dced0d935a41
Ken Nakanishi <ikyhn1.ken.n@gmail.com>
2018-09-06 05:10:26 +0900

    fix many bugs

---
+++ operators.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+import math
+import cmath
+
+
+I = np.array([[1, 0], [0, 1]])
+X = np.array([[0, 1], [1, 0]])
+Y = np.array([[0, -1], [1, 0]]) * 1j
+Z = np.array([[1, 0], [0, -1]])
+H = np.array([[1, 1], [1, -1]]) / math.sqrt(2)
+S = np.array([[1, 0], [0, 1j]])
+
+rx = lambda phi: math.cos(phi/2) * I - 1j * math.sin(phi/2) * X
+ry = lambda phi: math.cos(phi/2) * I - 1j * math.sin(phi/2) * Y
+rz = lambda phi: math.cos(phi/2) * I - 1j * math.sin(phi/2) * Z
+
+sqrt_not = np.array([[1 + 1j, 1 - 1j], [1 - 1j, 1 + 1j]]) / 2
+phase_shift = lambda phi: np.array([[1, 0], [0, cmath.exp(-1j * phi)]])
+g_phase_shift = lambda phi: cmath.exp(1j * phi)
+
+
+def swap():
+    operator = np.zeros((2, 2, 2, 2))
+    operator[0, 0, 0, 0] = 1
+    operator[0, 1, 1, 0] = 1
+    operator[1, 0, 0, 1] = 1
+    operator[1, 1, 1, 1] = 1
+    return operator
+
+
+def sqrt_swap():
+    operator = np.zeros((2, 2, 2, 2), dtype=np.complex128)
+    operator[0, 0, 0, 0] = 1
+    operator[0, 1, 0, 1] = (1 + 1j) / 2
+    operator[1, 0, 1, 0] = (1 + 1j) / 2
+    operator[0, 1, 1, 0] = (1 - 1j) / 2
+    operator[1, 0, 0, 1] = (1 - 1j) / 2
+    operator[1, 1, 1, 1] = 1
+    return operator
+
+
+swap = swap()
+sqrt_swap = sqrt_swap()
+
+
+if __name__ == '__main__':
+    print(swap.reshape((4,4)))
+    print(sqrt_swap.reshape((4,4)))
--- qupy.py
+++ qupy.py
@@ -3,8 +3,6 @@ from __future__ import division
 from __future__ import print_function
 import numpy as np
 import math
-import cmath
-import sys
 try:
     import cupy
 except:
@@ -14,28 +12,23 @@ except:
 class Qubits:
     def __init__(self, size, dtype=np.complex128, gpu=-1):
         if gpu >= 0:
-            try:
-                self.xp = cupy
-                self.xp.cuda.Device(gpu).use()
-            except:
-                print('Cupy does not exist.')
-                sys.exit(-1)
+            self.xp = cupy
+            self.xp.cuda.Device(gpu).use()
         else:
             self.xp = np
 
-        xp = self.xp
         self.size = size
         self.dtype = dtype
 
-        self.data = xp.zeros([2] * self.size, dtype=dtype)
+        self.data = self.xp.zeros([2] * self.size, dtype=dtype)
         self.data[tuple([0] * self.size)] = 1
 
-    def gate(self, operator, target, control=None, control_sub=None):
+    def gate(self, operator, target, control=None, control_0=None):
         '''
         :param (numpy.array or cupy.ndarray) operator: unitary operator
         :param (None or int or tuple of int) target: operated qubits
         :param (None or int or tuple of int) control: operate target qubits where all control qubits are 1
-        :param (None or int or tuple of int) control_sub: operate target qubits where all control qubits are 0
+        :param (None or int or tuple of int) control_0: operate target qubits where all control qubits are 0
         '''
         xp = self.xp
 
@@ -43,20 +36,20 @@ class Qubits:
             target = (target,)
         if isinstance(control, int):
             control = (control,)
+        if isinstance(control_0, int):
+            control_0 = (control_0,)
 
-        self.data = xp.array(self.data).astype(self.dtype).reshape([2] * int(math.log2(self.data.size)))
-        operator = xp.array(operator).astype(self.dtype).reshape([2] * int(math.log2(operator.size)))
+        self.data = xp.asarray(self.data, dtype=self.dtype).reshape([2] * int(math.log2(self.data.size)))
+        operator = xp.asarray(operator, dtype=self.dtype).reshape([2] * int(math.log2(operator.size)))
 
-        if operator.ndim != len(target) * 2:
-            print('You must set operator.size==exp(len(target)*2)')
-            sys.exit(-1)
+        assert operator.ndim == len(target) * 2, 'You must set operator.size==exp(len(target)*2)'
 
         c_slice = [slice(None)] * self.size
         if control is not None:
             for _c in control:
                 c_slice[_c] = slice(1, 2)
-        if control_sub is not None:
-            for _c in control_sub:
+        if control_0 is not None:
+            for _c in control_0:
                 c_slice[_c] = slice(0, 1)
 
         c_index = list(range(self.size))
@@ -65,49 +58,23 @@ class Qubits:
             t_index[_t] = self.size + i
         o_index = [*target, *list(range(self.size, self.size + len(target)))]
 
-        # use following code when numpy bug is removed
+        # Use following code when numpy bug is removed and cupy can use this einsum format.
         # self.data[c_slice] = xp.einsum(operator, o_index, self.data[c_slice], c_index, t_index)
-        '''
-        >>> np.einsum(
-            np.array([[[[[[[[[[[[[[[[1]]]]]]]]]]]]]]]]),
-            [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
-            np.array([[[[[[[[[[[[[[[[1]]]]]]]]]]]]]]]]),
-            [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
-            [0,1,2,3,4,5,6,7,8,9,10,14,15,16,17,18,19,20,21,22,23,24,25]
-        )
-        array([[[[[[[[[[[[[[[[[[[[[[[1]]]]]]]]]]]]]]]]]]]]]]])
-        
-        >>> np.einsum(
-            np.array([[[[[[[[[[[[[[[[[1]]]]]]]]]]]]]]]]]),
-            [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
-            np.array([[[[[[[[[[[[[[[[[1]]]]]]]]]]]]]]]]]),
-            [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
-            [0,1,2,3,4,5,6,7,8,9,10,14,15,16,17,18,19,20,21,22,23,24,25,26]
-        )
-        ValueError: invalid subscript '{' in einstein sum subscripts string, subscripts must be letters
-        '''
 
-        # alternative code
-        if (np.max(t_index) <= 25) and (xp == np):
-            self.data[c_slice] = xp.einsum(operator, o_index, self.data[c_slice], c_index, t_index)
-        else:
-            character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-            o_index = ''.join([character[i] for i in o_index])
-            c_index = ''.join([character[i] for i in c_index])
-            t_index = ''.join([character[i] for i in t_index])
-            subscripts = '{},{}->{}'.format(o_index, c_index, t_index)
-            self.data[c_slice] = xp.einsum(subscripts, operator, self.data[c_slice])
+        # Alternative code
+        character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        o_index = ''.join([character[i] for i in o_index])
+        c_index = ''.join([character[i] for i in c_index])
+        t_index = ''.join([character[i] for i in t_index])
+        subscripts = '{},{}->{}'.format(o_index, c_index, t_index)
+        self.data[c_slice] = xp.einsum(subscripts, operator, self.data[c_slice])
 
     def projection(self, target):
         xp = self.xp
 
         data = xp.split(self.data, [1], axis=target)
-        if xp == np:
-            p = [xp.sum(data[i] ** 2).real for i in (0, 1)]  # xp.square has bug when it get complex128's array
-            obs = xp.random.choice([0, 1], p=p)
-        else:
-            p = [np.asscalar(cupy.asnumpy(xp.sum(data[i] ** 2).real)) for i in (0, 1)]  # difference between numpy and cupy
-            obs = np.asscalar(cupy.asnumpy(xp.random.choice([0, 1], size=1, p=p)))
+        p = [self.to_scalar(xp.sum(data[i] * xp.conj(data[i])).real) for i in (0, 1)]
+        obs = self.to_scalar(xp.random.choice([0, 1], p=p))
 
         q_index = [1] * self.size
         q_index[target] = 2
@@ -120,27 +87,17 @@ class Qubits:
         if shape:
             print('shape: {}'.format(self.data.shape))
 
+    def to_scalar(self, x):
+        if self.xp != np:
+            if isinstance(x, cupy.ndarray):
+                x = cupy.asnumpy(x)
+        if isinstance(x, np.ndarray):
+            x = np.asscalar(x)
+        return x
+
 
 if __name__ == '__main__':
-    I = np.array([[1, 0], [0, 1]])
-    X = np.array([[0, 1], [1, 0]])
-    Y = np.array([[0, -1j], [1j, 0]])
-    Z = np.array([[1, 0], [0, -1]])
-    H = np.array([[1, 1], [1, -1]]) / math.sqrt(2)
-    S = np.array([[1, 0], [0, 1j]])
-
-    rx = lambda phi: math.cos(phi / 2) * I - 1j * math.sin(phi / 2) * X
-    ry = lambda phi: math.cos(phi / 2) * I - 1j * math.sin(phi / 2) * Y
-    rz = lambda phi: math.cos(phi / 2) * I - 1j * math.sin(phi / 2) * Z
-
-    sqrt_not = np.array([[1 + 1j, 1 - 1j], [1 - 1j, 1 + 1j]]) / 2
-    phase = lambda phi: np.array([[1, 0], [0, cmath.exp(1j * phi)]])
-    g_phase = lambda phi: cmath.exp(1j * phi)
-
-    swap = np.array([[1, 0, 0, 0],
-                     [0, 0, 1, 0],
-                     [0, 1, 0, 0],
-                     [0, 0, 0, 1]])
+    from operators import H, X, rz, swap
 
     iswap = np.array([[1, 0, 0, 0],
                       [0, 0, 1j, 0],
@@ -153,7 +110,13 @@ if __name__ == '__main__':
     q.info()
     q.gate(X, target=2, control=(0, 1))
     q.info()
+    q.gate(X, target=0, control=1, control_0=2)
+    q.info()
     q.gate(swap, target=(1, 2))
+    q.info()
+    q.gate(rz(np.pi / 4), target=1)
+    q.info()
+    q.gate(iswap, target=(1, 2))
     q.info()
     res = q.projection(target=1)
     print(res)
